ea69759f09c924d3d7ee874b9557a1c65465de40
Mangochops <whymangoeswillicent@gmail.com>
2025-07-19 14:37:28 +0300

    chore:Added a time up to keep the file running in the tiltfile

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"log"
 	"ride-sharing/services/trip-service/internal/domain"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"time"
 )
 
 func main() {
@@ -14,11 +16,20 @@ func main() {
 	tripService := service.NewTripService(repo)
 
 	// Use tripService for handling trip-related operations
-	tripService.CreateTrip(ctx, &domain.RideFareModel{
-		UserID:         "user123",
-		PackageSlug:    "standard",
+	fare := &domain.RideFareModel{
+		UserID:        "user123",
+		PackageSlug:   "standard",
 		TotalPriceInCents: 1000,
-	})
+	}
+	trip, err := tripService.CreateTrip(ctx, fare)
 
-	
+	if err != nil {
+		log.Fatalf("Failed to create trip: %v", err)
+	}
+	log.Printf("Trip created successfully: %+v", trip)
+	log.Printf("Ride Fare: %+v", trip.RideFare)
+
+	for {
+		time.Sleep(5 * time.Second)
+	}
 }
